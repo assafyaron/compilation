6f1df753b8bc9d03fffa3add2822ba0aaba44d7f
avivschiby1 <127945606+avivschiby1@users.noreply.github.com>
2024-12-09 11:10:35 +0200

    Update AST_PROGRAM.java

--- ex2/src/AST/AST_PROGRAM.java
+++ ex2/src/AST/AST_PROGRAM.java
@@ -1,1 +1,12 @@
+package AST;
 
+public abstract class AST_PROGRAM
+{
+	/*********************************************************/
+	/* The default message for an unknown AST statement node */
+	/*********************************************************/
+	public void PrintMe()
+	{
+		System.out.print("UNKNOWN AST STATEMENT NODE");
+	}
+}
