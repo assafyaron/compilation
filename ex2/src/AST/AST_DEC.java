69c5aabcce08212c2d2bc8a8d680cd940713207a
NoamShtrahman <127985394+NoamShtrahman@users.noreply.github.com>
2024-12-09 16:08:22 +0200

    Update AST_DEC.java

--- ex2/src/AST/AST_DEC.java
+++ ex2/src/AST/AST_DEC.java
@@ -1,12 +1,5 @@
 package AST;
 
-public class AST_DEC extends AST_PROGRAM
+public abstract class AST_DEC extends AST_Node
 {
-	/*********************************************************/
-	/* The default message for an unknown AST statement node */
-	/*********************************************************/
-	public void PrintMe()
-	{
-		System.out.print("UNKNOWN AST STATEMENT NODE");
-	}
 }
