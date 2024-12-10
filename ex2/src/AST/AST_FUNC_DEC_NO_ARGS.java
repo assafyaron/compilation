4f67fc04062b6cc83bc770382d1544601e1f374e
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-10 13:02:41 +0200

    Update AST_FUNC_DEC_NO_ARGS.java

--- ex2/src/AST/AST_FUNC_DEC_NO_ARGS.java
+++ ex2/src/AST/AST_FUNC_DEC_NO_ARGS.java
@@ -1,1 +1,63 @@
+package AST;
 
+public class AST_FUNC_DEC_NO_ARGS extends AST_EXP
+{
+    public AST_TYPE t;
+    public string name;
+    public AST_STMT_LIST body;
+
+    /******************/
+    /* CONSTRUCTOR(S) */
+    /******************/
+    public AST_FUNC_DEC_ARGS(AST_TYPE t, string name, AST_STMT_LIST body)
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.format("====================== funcDec -> func dec no args");
+
+        /*******************************/
+        /* COPY INPUT DATA MEMBERS ... */
+        /*******************************/
+        this.t1 = t1;
+        this.name1 = name;
+        this.body = body;
+
+
+    }
+
+    /***************************************************/
+    /* The printing message for a class declaration with inheritance AST node */
+    /***************************************************/
+    public void PrintMe()
+    {
+        /*********************************/
+        /* AST NODE TYPE = CLASS DEC INHERITANCE */
+        /*********************************/
+        System.out.println("AST NODE CLASS DEC INHERITANCE");
+
+        /******************************************/
+        /* PRINT class name1, name2, and clist rec... */
+        /******************************************/
+        System.out.format("CLASS 1 NAME( %s )\n", name1);
+        System.out.format("CLASS 2 NAME( %s )\n", name2);
+        if (cFR != null) cFR.PrintMe();
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            String.format("CLASS DEC\n EXTENDS\n...->%s", name1));
+
+        /****************************************/
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /****************************************/
+        if (cFR != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, cFR.SerialNumber);
+    }
+}
