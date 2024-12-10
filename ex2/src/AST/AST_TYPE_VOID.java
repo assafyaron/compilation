edc00a7c9af17f41a16a29388ff823166312f5f8
NoamShtrahman <127985394+NoamShtrahman@users.noreply.github.com>
2024-12-10 12:56:38 +0200

    Update AST_TYPE_VOID.java

--- ex2/src/AST/AST_TYPE_VOID.java
+++ ex2/src/AST/AST_TYPE_VOID.java
@@ -1,1 +1,40 @@
+package AST;
 
+public class AST_TYPE_VOID extends AST_TYPE
+{
+    /******************/
+    /* CONSTRUCTOR(S) */
+    /******************/
+    public AST_TYPE_VOID()
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.println("====================== type -> TYPE_VOID");
+
+    }
+
+    /***************************************************/
+    /* The printing message for a TYPE_VOID AST node */
+    /***************************************************/
+    public void PrintMe()
+    {
+        /*********************************/
+        /* AST NODE TYPE = TYPE_VOID */
+        /*********************************/
+        System.out.println("AST NODE TYPE_VOID");
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            "TYPE\nVOID");
+
+    }
+}
