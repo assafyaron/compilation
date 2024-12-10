e5719633533b7f93a8438cd3512b0086ad194551
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-10 15:15:07 +0200

    Update AST_FUNC_DEC_ARGS_LIST.java

--- ex2/src/AST/AST_FUNC_DEC_ARGS_LIST.java
+++ ex2/src/AST/AST_FUNC_DEC_ARGS_LIST.java
@@ -1,1 +1,66 @@
+package AST;
 
+public class AST_FUNC_DEC_ARGS_LIST extends AST_Node
+{
+	/****************/
+	/* DATA MEMBERS */
+	/****************/
+	public AST_TYPE t;
+  public string name;
+	public AST_FUNC_DEC_ARGS_LIST args;
+
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_FUNC_DEC_ARGS_LIST(AST_TYPE t, string name, AST_FUNC_DEC_ARGS_LIST args)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		if (args != null) System.out.print("====================== funcDecArgs -> comma type funcDecArgs\n");
+		if (args == null) System.out.print("====================== funcDecArgs -> comma type\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.t = t;
+		this.name = name;
+    this.args = args;
+	}
+
+	/******************************************************/
+	/* The printing message for a statement list AST node */
+	/******************************************************/
+	public void PrintMe()
+	{
+		/**************************************/
+		/* AST NODE TYPE = AST STATEMENT LIST */
+		/**************************************/
+		System.out.print("AST NODE STMT LIST\n");
+
+		/*************************************/
+		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/*************************************/
+		if (head != null) head.PrintMe();
+		if (tail != null) tail.PrintMe();
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"STMT\nLIST\n");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (head != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,head.SerialNumber);
+		if (tail != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,tail.SerialNumber);
+	}
+	
+}
