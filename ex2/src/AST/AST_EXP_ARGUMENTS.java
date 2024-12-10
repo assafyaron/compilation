290c859a331081f3affa221294e4d0b8449c0b82
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-10 14:54:04 +0200

    Update AST_EXP_ARGUMENTS.java

--- ex2/src/AST/AST_EXP_ARGUMENTS.java
+++ ex2/src/AST/AST_EXP_ARGUMENTS.java
@@ -1,1 +1,64 @@
+package AST;
 
+public class AST_EXP_ARGUMENTS extends AST_Node
+{
+	/****************/
+	/* DATA MEMBERS */
+	/****************/
+	public AST_EXP e;
+	public AST_EXP_ARGUMENTS eA;
+
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_STMT_LIST(AST_EXP e, AST_EXP_ARGUMENTS eA)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		if (tail != null) System.out.print("====================== expArguments -> exp expArguments\n");
+		if (tail == null) System.out.print("====================== expArguments -> exp\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.e = e;
+		this.eA = eA;
+	}
+
+	/******************************************************/
+	/* The printing message for a statement list AST node */
+	/******************************************************/
+	public void PrintMe()
+	{
+		/**************************************/
+		/* AST NODE TYPE = AST STATEMENT LIST */
+		/**************************************/
+		System.out.print("AST NODE STMT LIST\n");
+
+		/*************************************/
+		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/*************************************/
+		if (head != null) head.PrintMe();
+		if (tail != null) tail.PrintMe();
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"STMT\nLIST\n");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (head != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,head.SerialNumber);
+		if (tail != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,tail.SerialNumber);
+	}
+	
+}
