821cb04b92abf9da3099f371b9502e9eaeae3886
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-10 12:42:34 +0200

    Update AST_EXP_STRING.java

--- ex2/src/AST/AST_EXP_STRING.java
+++ ex2/src/AST/AST_EXP_STRING.java
@@ -1,1 +1,45 @@
+package AST;
 
+public class AST_EXP_STRING extends AST_EXP
+{
+	public string s;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_EXP_STRING(string s)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.format("====================== exp -> STRING( %s )\n", s);
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.s = s;
+	}
+
+	/************************************************/
+	/* The printing message for an INT EXP AST node */
+	/************************************************/
+	public void PrintMe()
+	{
+		/*******************************/
+		/* AST NODE TYPE = AST INT EXP */
+		/*******************************/
+		System.out.format("AST NODE INT( %d )\n",value);
+
+		/*********************************/
+		/* Print to AST GRAPHIZ DOT file */
+		/*********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("INT(%d)",value));
+	}
+}
