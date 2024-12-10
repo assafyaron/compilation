bc94812cd9a85332eb6e16f46c85a58975b7447e
NoamShtrahman <127985394+NoamShtrahman@users.noreply.github.com>
2024-12-10 12:40:25 +0200

    Update AST_VAR_DEC_ARGS.java

--- ex2/src/AST/AST_VAR_DEC_ARGS.java
+++ ex2/src/AST/AST_VAR_DEC_ARGS.java
@@ -1,1 +1,61 @@
+package AST;
 
+public class AST_VAR_DEC_ARGS extends AST_VAR_DEC
+{
+	public AST_TYPE type;
+	public AST_EXP exp;
+  public String name
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_VAR_DEC_ARGS(AST_TYPE type,AST_EXP exp, String name)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.print("====================== varDec -> type ID ASSIGN exp SEMICOLON\n");
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.type = type;
+		this.exp = exp;
+    this.name = name;
+	}
+
+	/*****************************************************/
+  /* The printing message for a variable declaration with type, ID, and expression AST node */
+	/*****************************************************/
+	public void PrintMe()
+	{
+		/*************************************/
+		/* AST NODE TYPE = AST SUBSCRIPT VAR */
+		/*************************************/
+		System.out.print("AST NODE SUBSCRIPT VAR\n");
+
+		/****************************************/
+		/* RECURSIVELY PRINT VAR + SUBSRIPT ... */
+		/****************************************/
+		if (var != null) var.PrintMe();
+		if (subscript != null) subscript.PrintMe();
+		
+		/***************************************/
+		/* PRINT Node to AST GRAPHVIZ DOT file */
+		/***************************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"SUBSCRIPT\nVAR\n...[...]");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (var       != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,var.SerialNumber);
+		if (subscript != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,subscript.SerialNumber);
+	}
+}
