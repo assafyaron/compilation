944fed5f92b6e18cfaea2f585616b5ba9b4e6b80
NoamShtrahman <127985394+NoamShtrahman@users.noreply.github.com>
2024-12-10 12:59:45 +0200

    Update AST_TYPE_ID.java

--- ex2/src/AST/AST_TYPE_ID.java
+++ ex2/src/AST/AST_TYPE_ID.java
@@ -1,1 +1,50 @@
+package AST;
 
+public class AST_TYPE_ID extends AST_TYPE
+{
+    public String name;
+
+    /******************/
+    /* CONSTRUCTOR(S) */
+    /******************/
+    public AST_TYPE_ID(String name)
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.format("====================== type -> ID( %s )\n", name);
+
+        /*******************************/
+        /* COPY INPUT DATA MEMBERS ... */
+        /*******************************/
+        this.name = name;
+    }
+
+    /***************************************************/
+    /* The printing message for a TYPE_ID AST node */
+    /***************************************************/
+    public void PrintMe()
+    {
+        /*********************************/
+        /* AST NODE TYPE = TYPE_ID */
+        /*********************************/
+        System.out.println("AST NODE TYPE ID");
+
+        /******************************************/
+        /* PRINT type name */
+        /******************************************/
+        System.out.format("TYPE NAME( %s )\n", name);
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            String.format("TYPE\nID\n...->%s", name));
+    }
+}
