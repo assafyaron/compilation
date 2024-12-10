246e378055749ee853c8e5efeec8ef88a4ccbbb1
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-10 12:11:24 +0200

    Update AST_DEC_ARRAY_TYPE_DEF.java

--- ex2/src/AST/AST_DEC_ARRAY_TYPE_DEF.java
+++ ex2/src/AST/AST_DEC_ARRAY_TYPE_DEF.java
@@ -1,1 +1,58 @@
+package AST;
 
+public class AST_ARRAY_TYPE_DEF extends AST_DEC
+{
+    public AST_VAR_DEC var_dec;
+
+    /******************/
+    /* CONSTRUCTOR(S) */
+    /******************/
+    public AST_ARRAY_TYPE_DE(AST_VAR_DEC var_dec)
+    {
+        /******************************/
+        /* SET A UNIQUE SERIAL NUMBER */
+        /******************************/
+        SerialNumber = AST_Node_Serial_Number.getFresh();
+
+        /***************************************/
+        /* PRINT CORRESPONDING DERIVATION RULE */
+        /***************************************/
+        System.out.format("====================== dec -> varDec");
+
+        /*******************************/
+        /* COPY INPUT DATA MEMBERS ... */
+        /*******************************/
+        this.var_dec = var_dec;
+
+    }
+
+    /***************************************************/
+    /* The printing message for a class declaration with inheritance AST node */
+    /***************************************************/
+    public void PrintMe()
+    {
+        /*********************************/
+        /* AST NODE TYPE = CLASS DEC INHERITANCE */
+        /*********************************/
+        System.out.println("AST NODE CLASS DEC INHERITANCE");
+
+        /******************************************/
+        /* PRINT class name1, name2, and clist rec... */
+        /******************************************/
+        System.out.format("CLASS 1 NAME( %s )\n", name1);
+        System.out.format("CLASS 2 NAME( %s )\n", name2);
+        if (cFR != null) cFR.PrintMe();
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            String.format("CLASS DEC\n EXTENDS\n...->%s", name1));
+
+        /****************************************/
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /****************************************/
+        if (cFR != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, cFR.SerialNumber);
+    }
+}
