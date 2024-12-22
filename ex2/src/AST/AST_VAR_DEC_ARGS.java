57c281d37c6f27ce9b7e06baf68f98ad1635ac77
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-22 19:22:00 +0200

    Update AST_VAR_DEC_ARGS.java

--- ex2/src/AST/AST_VAR_DEC_ARGS.java
+++ ex2/src/AST/AST_VAR_DEC_ARGS.java
@@ -2,15 +2,15 @@ package AST;
 
 public class AST_VAR_DEC_ARGS extends AST_VAR_DEC
 {
-	public AST_TYPE type;
+	public AST_TYPE t;
 	public String name;
-	public AST_EXP exp;
+	public AST_EXP e;
   	
 	
 	/******************/
 	/* CONSTRUCTOR(S) */
 	/******************/
-	public AST_VAR_DEC_ARGS(AST_TYPE type, String name, AST_EXP exp)
+	public AST_VAR_DEC_ARGS(AST_TYPE t, String name, AST_EXP e)
 	{
 		/******************************/
 		/* SET A UNIQUE SERIAL NUMBER */
@@ -25,10 +25,37 @@ public class AST_VAR_DEC_ARGS extends AST_VAR_DEC
 		/*******************************/
 		/* COPY INPUT DATA NENBERS ... */
 		/*******************************/
-		this.type = type;
+		this.t = t;
 		this.name = name;
-		this.exp = exp;
-    
+		this.e = e;
 	}
+
+	public void PrintMe()
+    {
+        /*********************************/
+        /* AST NODE TYPE = VAR DEC ARGS */
+        /*********************************/
+        System.out.println("AST NODE VAR DEC ARGS");
+
+        /******************************************/
+        /* RECURSIVELY PRINT t and e ... */
+        /******************************************/
+        if (t != null) t.PrintMe();
+	System.out.println("ID Name: " + name);
+        if (e != null) e.PrintMe();
+
+        /***************************************/
+        /* PRINT Node to AST GRAPHVIZ DOT file */
+        /***************************************/
+        AST_GRAPHVIZ.getInstance().logNode(
+            SerialNumber,
+            "VAR DEC\nARGS");
+
+        /****************************************/
+        /* PRINT Edges to AST GRAPHVIZ DOT file */
+        /****************************************/
+        if (t != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, t.SerialNumber);
+        if (e != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, e.SerialNumber);
+    }
 
 }
