f97dc4f5cf7d857542918cd86a26441e051a6391
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-22 18:52:52 +0200

    Update AST_STMT_LIST.java

--- ex2/src/AST/AST_STMT_LIST.java
+++ ex2/src/AST/AST_STMT_LIST.java
@@ -21,7 +21,7 @@ public class AST_STMT_LIST extends AST_Node
 		/***************************************/
 		/* PRINT CORRESPONDING DERIVATION RULE */
 		/***************************************/
-		if (tail != null) System.out.print("====================== stmts -> stmt stmts\n");
+		if (tail != null) System.out.print("====================== stmts -> stmt stmtList\n");
 		if (tail == null) System.out.print("====================== stmts -> stmt      \n");
 
 		/*******************************/
@@ -29,6 +29,33 @@ public class AST_STMT_LIST extends AST_Node
 		/*******************************/
 		this.head = head;
 		this.tail = tail;
+	}
+
+	public void PrintMe()
+	{
+		/**************************************/
+		/* AST NODE TYPE = AST STMT LIST */
+		/**************************************/
+		System.out.print("AST NODE STMT LIST\n");
+
+		/*************************************/
+		/* RECURSIVELY PRINT HEAD + TAIL ... */
+		/*************************************/
+		if (head != null) head.PrintMe();
+		if (tail != null) tail.PrintMe();
+
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			"STMT\nLIST\n");
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (head != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,head.SerialNumber);
+		if (tail != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,tail.SerialNumber);
 	}
 
 }
