8428b5ba1a1fee57935a73155f2d8d1d28bcef82
assafyaron <128134935+assafyaron@users.noreply.github.com>
2024-12-24 16:35:53 +0200

    Update AST_STMT_VAR_DEC.java
    DEC_VAR -> VAR_DEC
    
    copy from skeleton

--- ex3/src/AST/our_ast/AST_STMT_VAR_DEC.java
+++ ex3/src/AST/our_ast/AST_STMT_VAR_DEC.java
@@ -1,52 +1,46 @@
 package AST;
 
+import TYPES.*;
+
 public class AST_STMT_VAR_DEC extends AST_STMT
 {
-    public AST_VAR_DEC var_dec;
+	/****************/
+	/* DATA MEMBERS */
+	/****************/
+	public AST_DEC_VAR var;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_STMT_VAR_DEC(AST_DEC_VAR var)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
 
-    /******************/
-    /* CONSTRUCTOR(S) */
-    /******************/
-    public AST_STMT_VAR_DEC(AST_VAR_DEC var_dec)
-    {
-        /******************************/
-        /* SET A UNIQUE SERIAL NUMBER */
-        /******************************/
-        SerialNumber = AST_Node_Serial_Number.getFresh();
+		this.var = var;
+	}
+	
+	public TYPE SemantMe()
+	{
+		return var.SemantMe();
+	}
+	
+	public void PrintMe()
+	{
+		var.PrintMe();
 
-        /***************************************/
-        /* PRINT CORRESPONDING DERIVATION RULE */
-        /***************************************/
-        System.out.println("====================== stmt -> varDec");
-
-        /*******************************/
-        /* COPY INPUT DATA MEMBERS ... */
-        /*******************************/
-        this.var_dec = var_dec;
-    }
-
-    public void PrintMe()
-    {
-        /*********************************/
-        /* AST NODE TYPE = STMT VAR DEC */
-        /*********************************/
-        System.out.println("AST NODE STMT VAR DEC");
-
-        /******************************************/
-        /* RECURSIVELY PRINT var_dec ... */
-        /******************************************/
-        if (var_dec != null) var_dec.PrintMe();
-
-        /***************************************/
-        /* PRINT Node to AST GRAPHVIZ DOT file */
-        /***************************************/
-        AST_GRAPHVIZ.getInstance().logNode(
-            SerialNumber,
-            "STMT\nVAR DEC");
-
-        /****************************************/
-        /* PRINT Edges to AST GRAPHVIZ DOT file */
-        /****************************************/
-        if (var_dec != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, var_dec.SerialNumber);
-    }
+		/***************************************/
+		/* PRINT Node to AST GRAPHVIZ DOT file */
+		/***************************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("STMT\nVAR\nDEC"));
+		
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,var.SerialNumber);		
+	}
 }
