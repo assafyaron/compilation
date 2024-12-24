733f0a5d5d7e1257fe41ddd26b24553aedacb22c
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-24 17:12:21 +0200

    Update AST_NEW_EXP.java

--- ex3/src/AST/AST_NEW_EXP.java
+++ ex3/src/AST/AST_NEW_EXP.java
@@ -1,4 +1,7 @@
 package AST;
 
-public abstract class AST_NEW_EXP extends AST_Node {   
+public abstract class AST_NEW_EXP extends AST_Node { 
+   public TYPE SemantMe() {
+        return null;
+    }
 }
