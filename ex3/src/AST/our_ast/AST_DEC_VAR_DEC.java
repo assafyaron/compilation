efb2737c1a64fc16ea16cccd102b23c1a0083e67
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-24 16:34:34 +0200

    Update AST_DEC_VAR_DEC.java

--- ex3/src/AST/our_ast/AST_DEC_VAR_DEC.java
+++ ex3/src/AST/our_ast/AST_DEC_VAR_DEC.java
@@ -1,54 +1,93 @@
 package AST;
 
+import TYPES.*;
+import SYMBOL_TABLE.*;
+
 public class AST_DEC_VAR_DEC extends AST_DEC
 {
-    public AST_VAR_DEC var_dec;
+	/****************/
+	/* DATA MEMBERS */
+	/****************/
+	public String type;
+	public String name;
+	public AST_EXP initialValue;
+	
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_DEC_VAR_DEC(String type,String name,AST_EXP initialValue)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
 
-    /******************/
-    /* CONSTRUCTOR(S) */
-    /******************/
-    public AST_DEC_VAR_DEC(AST_VAR_DEC var_dec)
-    {
-        /******************************/
-        /* SET A UNIQUE SERIAL NUMBER */
-        /******************************/
-        SerialNumber = AST_Node_Serial_Number.getFresh();
+		this.type = type;
+		this.name = name;
+		this.initialValue = initialValue;
+	}
 
-        /***************************************/
-        /* PRINT CORRESPONDING DERIVATION RULE */
-        /***************************************/
-        System.out.format("====================== dec -> varDec\n");
+	/********************************************************/
+	/* The printing message for a declaration list AST node */
+	/********************************************************/
+	public void PrintMe()
+	{
+		/********************************/
+		/* AST NODE TYPE = AST DEC LIST */
+		/********************************/
+		if (initialValue != null) System.out.format("VAR-DEC(%s):%s := initialValue\n",name,type);
+		if (initialValue == null) System.out.format("VAR-DEC(%s):%s                \n",name,type);
 
-        /*******************************/
-        /* COPY INPUT DATA MEMBERS ... */
-        /*******************************/
-        this.var_dec = var_dec;
+		/**************************************/
+		/* RECURSIVELY PRINT initialValue ... */
+		/**************************************/
+		if (initialValue != null) initialValue.PrintMe();
 
-    }
+		/**********************************/
+		/* PRINT to AST GRAPHVIZ DOT file */
+		/**********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("VAR\nDEC(%s)\n:%s",name,type));
 
-    public void PrintMe()
-    {
-        /*********************************/
-        /* AST NODE TYPE = DEC VAR DEC */
-        /*********************************/
-        System.out.println("AST NODE DEC VAR DEC");
+		/****************************************/
+		/* PRINT Edges to AST GRAPHVIZ DOT file */
+		/****************************************/
+		if (initialValue != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber,initialValue.SerialNumber);		
+			
+	}
 
-        /******************************************/
-        /* RECURSIVELY PRINT var_dec ... */
-        /******************************************/
-        if (var_dec != null) var_dec.PrintMe();
+	public TYPE SemantMe()
+	{
+		TYPE t;
+	
+		/****************************/
+		/* [1] Check If Type exists */
+		/****************************/
+		t = SYMBOL_TABLE.getInstance().find(type);
+		if (t == null)
+		{
+			System.out.format(">> ERROR [%d:%d] non existing type %s\n",2,2,type);
+			System.exit(0);
+		}
+		
+		/**************************************/
+		/* [2] Check That Name does NOT exist */
+		/**************************************/
+		if (SYMBOL_TABLE.getInstance().find(name) != null)
+		{
+			System.out.format(">> ERROR [%d:%d] variable %s already exists in scope\n",2,2,name);				
+		}
 
-        /***************************************/
-        /* PRINT Node to AST GRAPHVIZ DOT file */
-        /***************************************/
-        AST_GRAPHVIZ.getInstance().logNode(
-            SerialNumber,
-            "DEC\nVAR DEC");
+		/***************************************************/
+		/* [3] Enter the Function Type to the Symbol Table */
+		/***************************************************/
+		SYMBOL_TABLE.getInstance().enter(name,t);
 
-        /****************************************/
-        /* PRINT Edges to AST GRAPHVIZ DOT file */
-        /****************************************/
-        if (var_dec != null) AST_GRAPHVIZ.getInstance().logEdge(SerialNumber, var_dec.SerialNumber);
-    }
-
+		/*********************************************************/
+		/* [4] Return value is irrelevant for class declarations */
+		/*********************************************************/
+		return null;		
+	}
+	
 }
