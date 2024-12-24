6e4e3e6fe984acfa7fbee5bd55e0be9ec1b8369b
YonatanStrauss4 <yonatanstr123@gmail.com>
2024-12-24 17:13:00 +0200

    Update AST_FUNC_STMT.java

--- ex3/src/AST/AST_FUNC_STMT.java
+++ ex3/src/AST/AST_FUNC_STMT.java
@@ -1,4 +1,7 @@
 package AST;
 
-public abstract class AST_FUNC_STMT extends AST_Node {   
+public abstract class AST_FUNC_STMT extends AST_Node {  
+   public TYPE SemantMe() {
+        return null;
+    }
 }
